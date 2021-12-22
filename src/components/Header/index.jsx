82ab8fc30e649532e0437a66635dee07a2d99ed6
Johann <johanncf@metalmat.ufrj.br>
2021-12-22 18:40:26 -0300

    Adapting to mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,26 @@
 import { BiCaretRightCircle } from "react-icons/bi";
-
+import { BiMenu, BiX } from "react-icons/bi";
+import { useState } from "react"
+ 
 export default function Header() {
+    const [display, setDisplay] = useState(false)
+
+    const handleDisplay = () => {
+        setDisplay(display => !display)
+    }
+
     return (
         <header className="page_header">
             <nav className="page_nav">
-                <ul className="nav_list">
-                    <button className="nav-button">
-                        <li className="nav-item">
-                            <BiCaretRightCircle id="logo-icon"/><span className="logo-name">CINE JS</span>
-                        </li>
-                    </button>
+                <button className={!display ? 'logo-button show' : 'logo-button'}>
+                    <BiCaretRightCircle className="logo-icon" /><span className="logo-name">CINE JS</span>
+                </button>
+                <button className="nav-button nav-item menu-button" onClick={handleDisplay}>
+                    {
+                        display ? <BiX className="menu-icon menu-open" /> : <BiMenu className="menu-icon" />
+                    }
+                </button>
+                <ul className={display ? 'nav_list show' : 'nav_list'}>
                     <button className="nav-button"><li className="nav-item">Início</li></button>
                     <button className="nav-button"><li className="nav-item">Séries</li></button>
                     <button className="nav-button"><li className="nav-item">Filmes</li></button>
